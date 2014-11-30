6269fada2e449b12115a6188d50402c7f8565a0f
yolo2013 <i_vagrancy@163.com>
2014-11-30 21:13:58 +0800

    更新正式url
    更新url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var list = ['java', '.net', 'php'];
 
 
 program
-    .version('0.0.2')
+    .version('0.0.4')
     .option('-p, --platform [platform]', 'platform which you are')
     .option('-l, --language [language]', 'language of generate files')
     .parse(process.argv);
@@ -21,20 +21,7 @@ program
 var platform = program.platform || 'java';
 var language = program.language || 'chin';
 
-mkdir('-p', 'release/');
-cd('release');
+var libUrl = 'git://192.168.84.25/generics.git';
 
-var libUrl;
-
-switch(platform) {
-case 'java':
-    libUrl = 'git@192.168.4.125:D:/Generics';
-    break;
-case '.net':
-    libUrl = 'https://github.com/JimLiu/angular-ui-tree';
-    break;
-case 'php':
-    break;
-}
 
 exec('git clone ' + libUrl);
